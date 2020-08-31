e43a09772664cc50d600b9cb4b8b8bd143157aa5
yafanisonya <yafanison@gmail.com>
2020-08-31 19:32:24 +0800

    request 请求获取天气

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,2 +1,43 @@
-// pages/list/list.js
-Page({})
+const dayMap = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+
+Page({
+    date:{
+        weekWeather:[]
+    },
+    onLoad(){
+        this.getWeekWeather()
+    },
+    getWeekWeather(callback){
+        wx.request({
+          url: 'https://test-miniprogram.com/api/weather/future',
+          data:{
+              time: new Date().getTime(),
+              city:'北京市'
+          },
+          success: res=>{
+              let result = res.data.result
+              this.setWeekWeather(result)
+          },
+          complete:()=>{
+              callback && callback()
+          }
+        })
+    },
+    setWeekWeather(result){
+        let weekWeather = []
+        for(let i=0;i<7;i++){
+            let date = new Date()
+            date.setDate(date.getDate()+i)
+            weekWeather.push({
+                day: dayMap[date.getDay()],
+                date:`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`,
+                temp:`${result[i].minTemp}° - ${result[i].maxTemp}°`,
+                iconPath:'/images/' + result[i].weather + '-icon.png'
+            })
+        }
+        weekWeather[0].day = '今天'
+        this.setData({
+            weekWeather:weekWeather
+        })
+    }
+})
