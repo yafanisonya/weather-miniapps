8541b950fc4b032a810a3b5f26c973a999d5f454
yafanisonya <yafanison@gmail.com>
2020-08-31 18:56:28 +0800

    首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,60 +1,92 @@
 const weatherMap = {
-    'sunny':'晴天',
+    'sunny': '晴天',
     'cloudy': '多云',
     'overcast': '阴',
     'lightrain': '小雨',
     'heavyrain': '大雨',
     'snow': '雪'
-}
-
-const weatherColorMap = {
+  }
+  
+  const weatherColorMap = {
     'sunny': '#cbeefd',
     'cloudy': '#deeef6',
     'overcast': '#c6ced2',
     'lightrain': '#bdd5e1',
     'heavyrain': '#c5ccd0',
     'snow': '#aae1fc'
-   }
-
-Page({
-    data:{
-        nowTemp:'',
-        nowWeather:'',
-        nowWeatherBackground:''
+  }
+  
+  Page({
+    data: {
+      nowTemp: '',
+      nowWeather: '',
+      nowWeatherBackground: "",
+      hourlyWeather: [],
+      todayTemp: "",
+      todayDate: ""
+    },
+    onLoad() {
+      this.getNow()
     },
     onPullDownRefresh(){
-        this.getNow(()=>{
-            wx.stopPullDownRefresh()
-        })
-    },
-    onLoad(){
-        this.getNow()
+      this.getNow(() => {
+        wx.stopPullDownRefresh()
+      })
     },
     getNow(callback){
-        wx.request({
-            url: 'https://test-miniprogram.com/api/weather/now', 
-            data: {
-              city:'邯郸市'
-            },
-            success: res => {
-              console.log(res)
-              let result = res.data.result
-              let temp = result.now.temp
-              let weather = result.now.weather
-              console.log(temp,weather)
-              this.setData({
-                  nowTemp: temp + '°',
-                  nowWeather:weatherMap[weather],
-                  nowWeatherBackground:'/images/' + weather + '-bg.png'
-              })
-              wx.setNavigationBarColor({
-                frontColor: '#000000',
-                backgroundColor: weatherColorMap[weather],
-              })
-            },
-            complete:()=>{
-                callback && callback()
-            }
-          })
+      wx.request({
+        url: 'https://test-miniprogram.com/api/weather/now',
+        data: {
+          city: '广州市'
+        },
+        success: res => {
+          let result = res.data.result
+          this.setNow(result)
+          this.setHourlyWeather(result)
+          this.setToday(result)
+        },
+        complete: () =>{
+          callback && callback()
+        }
+      })
+    },
+    setNow(result){
+      let temp = result.now.temp
+      let weather = result.now.weather
+      this.setData({
+        nowTemp: temp + '°',
+        nowWeather: weatherMap[weather],
+        nowWeatherBackground: '/images/' + weather + '-bg.png'
+      })
+      wx.setNavigationBarColor({
+        frontColor: '#000000',
+        backgroundColor: weatherColorMap[weather],
+      })
+    },
+    setHourlyWeather(result){
+      let forecast = result.forecast
+      let hourlyWeather = []
+      let nowHour = new Date().getHours()
+      for (let i = 0; i < 8; i += 1) {
+        hourlyWeather.push({
+          time: (i*3 + nowHour) % 24 + "时",
+          iconPath: '/images/' + forecast[i].weather + '-icon.png',
+          temp: forecast[i].temp + '°'
+        })
+      }
+      hourlyWeather[0].time = '现在'
+      this.setData({
+        hourlyWeather: hourlyWeather
+      })
+    },
+    setToday(result) {
+      let date = new Date()
+      this.setData({
+        todayTemp: `${result.today.minTemp}° - ${result.today.maxTemp}°`,
+        todayDate: `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} 今天`
+      })
+    },
+    onTapDayWeather(){
+      wx.showToast()
     }
-})
+  })
