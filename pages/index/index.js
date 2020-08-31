f5c10c927f55f806d15020f58cfa2f1873257816
yafanisonya <yafanison@gmail.com>
2020-08-31 16:26:28 +0800

    使用网络数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,60 @@
-Page({})
+const weatherMap = {
+    'sunny':'晴天',
+    'cloudy': '多云',
+    'overcast': '阴',
+    'lightrain': '小雨',
+    'heavyrain': '大雨',
+    'snow': '雪'
+}
+
+const weatherColorMap = {
+    'sunny': '#cbeefd',
+    'cloudy': '#deeef6',
+    'overcast': '#c6ced2',
+    'lightrain': '#bdd5e1',
+    'heavyrain': '#c5ccd0',
+    'snow': '#aae1fc'
+   }
+
+Page({
+    data:{
+        nowTemp:'',
+        nowWeather:'',
+        nowWeatherBackground:''
+    },
+    onPullDownRefresh(){
+        this.getNow(()=>{
+            wx.stopPullDownRefresh()
+        })
+    },
+    onLoad(){
+        this.getNow()
+    },
+    getNow(callback){
+        wx.request({
+            url: 'https://test-miniprogram.com/api/weather/now', 
+            data: {
+              city:'邯郸市'
+            },
+            success: res => {
+              console.log(res)
+              let result = res.data.result
+              let temp = result.now.temp
+              let weather = result.now.weather
+              console.log(temp,weather)
+              this.setData({
+                  nowTemp: temp + '°',
+                  nowWeather:weatherMap[weather],
+                  nowWeatherBackground:'/images/' + weather + '-bg.png'
+              })
+              wx.setNavigationBarColor({
+                frontColor: '#000000',
+                backgroundColor: weatherColorMap[weather],
+              })
+            },
+            complete:()=>{
+                callback && callback()
+            }
+          })
+    }
+})
